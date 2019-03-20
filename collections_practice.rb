c08a3e5aa15b448c8db2f5c087bfb4ae01b1d630
Savannah C <sgcorrell@gmail.com>
2019-03-20 17:29:49 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|i| i.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  kesha = []
+  array.each do |dollar|
+    dollar[2] = "$"
+    kesha << dollar
+  end
+  return kesha
+end
+
+def find_a(array)
+  array.select {|x| x.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.collect do |index|
+    if array[1] == index
+      index
+    else
+      index + "s"
+    end
+  end
+end
